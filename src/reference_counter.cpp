d8e61adeeb5f875ae73decfd5001a2c1411128b8
raunakbhagat <n1z1b>
2020-04-05 21:20:41 -0600

    added limiter functionality.

--- src/reference_counter.cpp
+++ src/reference_counter.cpp
@@ -6,10 +6,10 @@
 
 template<class T> class limit_ptr {
     private:
-        int * count = new int(1);
+        std::pair<int,int const> * count = new std::pair<int, int const>(1,-1);
         T * subject = nullptr;
 
-        int * const get_count_ptr() const;
+        std::pair<int,int const> * const get_count_ptr() const;
         T * const get_subject_ptr() const;
 
         void increment_count() const;
@@ -18,6 +18,7 @@ template<class T> class limit_ptr {
     public:
         limit_ptr();
         limit_ptr(T * const subject);
+        limit_ptr(T * const subject, int const limit);
         limit_ptr(limit_ptr<T> const & other);
         ~limit_ptr();
 
@@ -28,22 +29,23 @@ template<class T> class limit_ptr {
         T & operator*() const;
 
         int const getCount() const;
+        int const getLimit() const;
 
         void deepCopy();
         void deepCopy(limit_ptr<T> const & other);
 };
 
-template<class T> int * const limit_ptr<T>::get_count_ptr() const { return this->count; }
+template<class T> std::pair<int,int const> * const limit_ptr<T>::get_count_ptr() const { return this->count; }
 template<class T> T * const limit_ptr<T>::get_subject_ptr() const { return this->subject; }
 
 template<class T> void limit_ptr<T>::increment_count() const {
-    if (this->count) ++(*(this->count));
+    if (this->count) ++(this->count->first);
     return;
 }
 template<class T> void limit_ptr<T>::decrement_count() {
-    if (this->count) --(*(this->count));
+    if (this->count) --(this->count->first);
 
-    if (!(*(this->count))) {
+    if (!(this->count->first)) {
         delete this->count;
         delete this->subject;
 
@@ -56,6 +58,7 @@ template<class T> void limit_ptr<T>::decrement_count() {
 
 template<class T> limit_ptr<T>::limit_ptr() { return; }
 template<class T> limit_ptr<T>::limit_ptr(T * const subject) : subject(subject) { return; }
+template<class T> limit_ptr<T>::limit_ptr(T * const subject, int const limit) : count(new std::pair<int,int const>(1,((limit >= 1) ? limit : -1))), subject(subject) { return; }
 template<class T> limit_ptr<T>::limit_ptr(limit_ptr<T> const & other) : count(other.get_count_ptr()), subject(other.get_subject_ptr()) { this->increment_count(); return; }
 template<class T> limit_ptr<T>::~limit_ptr() { decrement_count(); return; }
 
@@ -72,17 +75,21 @@ template<class T> bool const limit_ptr<T>::operator!=(limit_ptr<T> const & other
 template<class T> T * const limit_ptr<T>::operator->() const { return this->subject; }
 template<class T> T & limit_ptr<T>::operator*() const { return *(this->subject); }
 
-template<class T> int const limit_ptr<T>::getCount() const { return *(this->count); }
+template<class T> int const limit_ptr<T>::getCount() const { return this->count->first; }
+template<class T> int const limit_ptr<T>::getLimit() const { return this->count->second; }
+
 template<class T> void limit_ptr<T>::deepCopy() {
     this->decrement_count();
-    this->count = new int(1);
+    this->count = new std::pair<int,int const>(*(this->count));
+    this->count->first = 1;
     this->subject = new T(*(this->subject));
 
     return;
 }
 template<class T> void limit_ptr<T>::deepCopy(limit_ptr<T> const & other) {
     this->decrement_count();
-    this->count = new int(1);
+    this->count = new std::pair<int,int const>(*(this->count));
+    this->count->first = 1;
     this->subject = new T(*(other.get_subject_ptr()));
 
     return;
