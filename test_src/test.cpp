716119bd06dc2249e070ebd706fd2dce257c1bd1
raunakbhagat <n1z1b>
2020-04-06 14:55:25 -0600

    committing and pushing changes

--- test_src/test.cpp
+++ test_src/test.cpp
@@ -35,34 +35,38 @@ void A::setX(int const x) { this->x = x; return; }
 int const A::getX() const { return this->x; }
 
 
-int main() {
-    limit_ptr<A> a(new A(1));
-    limit_ptr<A> b(new A(2));
+// int main() {
+//     limit_ptr<A> a(new A(1));
+//     limit_ptr<A> b(new A(2));
     
-    int a_x;
-    int b_x;
+//     int a_x;
+//     int b_x;
 
-    a_x = a->getX();
-    b_x = b->getX();
-    printf("a_x:\t%i\n",a_x);
-    printf("b_x:\t%i\n",b_x);
+//     a_x = a->getX();
+//     b_x = b->getX();
+//     printf("a_x:\t%i\n",a_x);
+//     printf("b_x:\t%i\n",b_x);
 
-    b = a;
+//     b = a;
 
-    a_x = a->getX();
-    b_x = b->getX();
-    printf("a_x:\t%i\n",a_x);
-    printf("b_x:\t%i\n",b_x);
+//     a_x = a->getX();
+//     b_x = b->getX();
+//     printf("a_x:\t%i\n",a_x);
+//     printf("b_x:\t%i\n",b_x);
 
-    b.deepCopy();
-    b->setX(22);
+//     b.deepCopy();
+//     b->setX(22);
 
-    a_x = a->getX();
-    b_x = b->getX();
-    printf("a_x:\t%i\n",a_x);
-    printf("b_x:\t%i\n",b_x);
+//     a_x = a->getX();
+//     b_x = b->getX();
+//     printf("a_x:\t%i\n",a_x);
+//     printf("b_x:\t%i\n",b_x);
 
-    printf("About to exit main.\n");
+//     printf("About to exit main.\n");
 
-    return 0;
+//     return 0;
+// }
+
+int main() {
+    limit_ptr<int> a(new int(5));
 }
